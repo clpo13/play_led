6247bf9d07f1599625edeaa140fad8f05e2aa21a
Cody Logan <clpo13@gmail.com>
2018-07-19 15:55:09 -0700

    Add command line flags using docopt

--- play_led.py
+++ play_led.py
@@ -14,16 +14,40 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+"""Usage:
+    play_led.py [-v] [-l PIN] [-b PIN] FILE
+    play_led.py (-h | --help)
+    play_led.py --version
+
+Arguments:
+    FILE  audio file to play
+
+Options:
+    -h --help            show this help message and exit
+    --version            show version and exit
+    -v --verbose         print more information
+    -l PIN --led=PIN     set the GPIO pin for the LED [default: 17]
+    -b PIN --button=PIN  set the GPIO pin for the button [default: 27]
+
+"""
+
 import pygame, sys
 from gpiozero import Button, LED
+from docopt import docopt
+
+arguments = docopt(__doc__, version='0.0.1')
 
 # initialize PyGame mixer
 pygame.mixer.init()
+print(arguments)
 
 # take command line input if present
-led_gpio = int(sys.argv[1]) if len(sys.argv) >= 2 else 17
-btn_gpio = int(sys.argv[2]) if len(sys.argv) >= 3 else 27
-snd_file = sys.argv[3] if len(sys.argv) >= 4 else 'drum_roll.ogg'
+#led_gpio = int(sys.argv[1]) if len(sys.argv) >= 2 else 17
+#btn_gpio = int(sys.argv[2]) if len(sys.argv) >= 3 else 27
+#snd_file = sys.argv[3] if len(sys.argv) >= 4 else 'drum_roll.ogg'
+led_gpio = int(arguments['--led']) if not arguments['--led'] == None else 17
+btn_gpio = int(arguments['--button']) if not arguments['--button'] == None else 27
+snd_file = arguments['FILE']
 
 # set GPIO pins
 led = LED(led_gpio)
@@ -41,9 +65,9 @@ while True:
     pygame.mixer.music.play()
     led.on()
 
-    # check if audio is still playing
+    # check if audio is still playing and wait for it to finish
     while pygame.mixer.music.get_busy() == True:
-        continue
+        pygame.time.wait(100)
 
     # audio done, turn led off
     led.off()
