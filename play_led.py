7d788dacecb51881fe10bf65395e03ca0c7a3955
Cody Logan <clpo13@gmail.com>
2019-10-02 13:28:58 -0700

    Add license identifier

--- play_led.py
+++ play_led.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Copyright (C) 2018 Cody Logan
+# Copyright (C) 2018-2019 Cody Logan
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -10,7 +11,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
